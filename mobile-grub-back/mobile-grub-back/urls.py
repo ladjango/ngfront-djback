62874883e583040a0901967dab31fcdbf4b74a2f
Marcel Chastain <marcel.chastain@gmail.com>
2016-05-05 14:05:09 -0700

    step4 fix for urls

--- mobile-grub-back/mobile-grub-back/urls.py
+++ mobile-grub-back/mobile-grub-back/urls.py
@@ -4,7 +4,6 @@ from django.contrib import admin
 
 
 urlpatterns = [
-    url(r'^$', views.index, name="index"),
     url(r'^', include('menu.urls')),
     url(r'^admin/', include(admin.site.urls)),
 ]
